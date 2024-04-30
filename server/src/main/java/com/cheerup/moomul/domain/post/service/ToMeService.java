d6700f31803dbdde2c05181a4d1a36523b715274
정유경 <yyk834@naver.com>
2024-04-30 10:49:29 +0900

    Feat : ✨댓글, 대댓글 기능 API

--- server/src/main/java/com/cheerup/moomul/domain/post/service/ToMeService.java
+++ server/src/main/java/com/cheerup/moomul/domain/post/service/ToMeService.java
@@ -5,11 +5,17 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cheerup.moomul.domain.member.entity.User;
 import com.cheerup.moomul.domain.member.repository.UserRepository;
+import com.cheerup.moomul.domain.post.dto.CommentRequestDto;
 import com.cheerup.moomul.domain.post.dto.CommentResponseDto;
 import com.cheerup.moomul.domain.post.dto.PostCommentRequestParam;
+import com.cheerup.moomul.domain.post.entity.Comment;
+import com.cheerup.moomul.domain.post.entity.Post;
 import com.cheerup.moomul.domain.post.repository.CommentRepository;
 import com.cheerup.moomul.domain.post.repository.PostRepository;
+import com.cheerup.moomul.global.response.BaseException;
+import com.cheerup.moomul.global.response.ErrorCode;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,5 +32,27 @@ public class ToMeService {
 
 	public List<CommentResponseDto> getComments(Long postId, PostCommentRequestParam param) {
 		return commentRepository.findCommentByPostId(postId, param);
+	}
+
+	@Transactional
+	public void createComments(User user, Long tomeId, CommentRequestDto requestDto) {
+		//현재 로그인 user, 게시글 userId, 게시글 Id
+
+		Post post = postRepository.findById(tomeId).orElseThrow(() -> new BaseException(ErrorCode.NO_POST_ERROR));
+		Comment parent = null;
+		if (requestDto.parentId() != null) {
+			parent = commentRepository.findById(requestDto.parentId())
+				.orElseThrow(() -> new BaseException(ErrorCode.NO_COMMENT_ERROR));
+		}
+		Comment comment = Comment.builder()
+			.user(user)
+			.post(post)
+			.content(requestDto.content())
+			.build();
+
+		if (parent != null) {
+			comment.addParent(parent);
+		}
+		commentRepository.save(comment);
 	}
 }
