f5ec65b9c45ac6797a041fa88812f3e2a6aae559
Seongeuniii <kse8538@naver.com>
2024-05-02 12:34:36 +0900

    Chore : 🧹 styled-reset 설치

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,13 @@
 import { RouterProvider } from 'react-router-dom';
 import router from '@/router/AppRouter';
+import { RecoilRoot } from 'recoil';
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <RecoilRoot>
+      <RouterProvider router={router} />
+    </RecoilRoot>
+  );
 }
 
 export default App;
