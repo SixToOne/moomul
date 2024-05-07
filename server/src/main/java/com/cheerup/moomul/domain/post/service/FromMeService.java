6a748d9350d02d316dfe58dfc7fad9597233ec9a
정유경 <yyk834@naver.com>
2024-05-07 13:48:06 +0900

    Feat : ✨ Fromme 댓글 조회, 작성

--- server/src/main/java/com/cheerup/moomul/domain/post/service/FromMeService.java
+++ server/src/main/java/com/cheerup/moomul/domain/post/service/FromMeService.java
@@ -10,15 +10,20 @@ import org.springframework.transaction.annotation.Transactional;
 import com.cheerup.moomul.domain.member.entity.User;
 import com.cheerup.moomul.domain.member.entity.UserDetailDto;
 import com.cheerup.moomul.domain.member.repository.UserRepository;
+import com.cheerup.moomul.domain.post.dto.CommentRequestDto;
+import com.cheerup.moomul.domain.post.dto.CommentResponseDto;
+import com.cheerup.moomul.domain.post.dto.PostCommentRequestParam;
 import com.cheerup.moomul.domain.post.dto.PostLikeResponseDto;
 import com.cheerup.moomul.domain.post.dto.PostRequestDto;
 import com.cheerup.moomul.domain.post.dto.PostResponseDto;
 import com.cheerup.moomul.domain.post.dto.VoteRequestDto;
+import com.cheerup.moomul.domain.post.entity.Comment;
 import com.cheerup.moomul.domain.post.entity.Option;
 import com.cheerup.moomul.domain.post.entity.Post;
 import com.cheerup.moomul.domain.post.entity.PostLike;
 import com.cheerup.moomul.domain.post.entity.PostType;
 import com.cheerup.moomul.domain.post.entity.Vote;
+import com.cheerup.moomul.domain.post.repository.CommentRepository;
 import com.cheerup.moomul.domain.post.repository.OptionRepository;
 import com.cheerup.moomul.domain.post.repository.PostLikeRepository;
 import com.cheerup.moomul.domain.post.repository.PostRepository;
@@ -39,6 +44,7 @@ public class FromMeService {
 	private final PostLikeRepository postLikeRepository;
 	private final VoteRepository voteRepository;
 	private final UserRepository userRepository;
+	private final CommentRepository commentRepository;
 
 	@Transactional
 	public void createFromMe(Long userId, PostRequestDto postRequestDto) {
@@ -153,5 +159,31 @@ public class FromMeService {
 		} else {
 			throw new BaseException(ErrorCode.NO_OPTION_ERROR);
 		}
+	}
+
+	public List<CommentResponseDto> getComments(Long postId, PostCommentRequestParam param) {
+		return commentRepository.findCommentByPostId(postId, param);
+	}
+
+	@Transactional
+	public void createComments(UserDetailDto user, Long tomeId, CommentRequestDto requestDto) {
+		//현재 로그인 user, 게시글 userId, 게시글 Id
+
+		Post post = postRepository.findById(tomeId, PostType.FROM_ME)
+			.orElseThrow(() -> new BaseException(ErrorCode.NO_POST_ERROR));
+		Comment parent = null;
+		if (requestDto.parentId() != null) {
+			parent = commentRepository.findById(requestDto.parentId())
+				.orElseThrow(() -> new BaseException(ErrorCode.NO_COMMENT_ERROR));
+		}
+		User loginUser = userRepository.findById(user.Id())
+			.orElseThrow(() -> new BaseException(ErrorCode.NO_USER_ERROR));
+
+		Comment comment = Comment.builder().user(loginUser).post(post).content(requestDto.content()).build();
+
+		if (parent != null) {
+			comment.addParent(parent);
+		}
+		commentRepository.save(comment);
 	}
 }
