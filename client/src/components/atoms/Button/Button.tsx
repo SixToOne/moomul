713e0dd633aab44b21ef05b8942a53eb1d9dd541
Seongeuniii <kse8538@naver.com>
2024-05-07 16:20:13 +0900

    Feat : ✨ 공통 버튼

---
+++ client/src/components/atoms/Button/Button.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface StyledProps {
+  width?: string;
+  height?: string;
+}
+
+interface ButtonProps extends StyledProps {
+  content: string;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+const Button = ({ content, onClick, ...styledProps }: ButtonProps) => {
+  return (
+    <StyledButton onClick={onClick} {...styledProps}>
+      {content}
+    </StyledButton>
+  );
+};
+
+const StyledButton = styled.button<StyledProps>`
+  width: ${({ width }) => (width ? width : '100%')};
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.PRIMARY};
+  color: ${({ theme }) => theme.WHITE};
+  font-size: 16px;
+  font-weight: 500;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.PRIMARY_DARK};
+  }
+`;
+
+export default Button;
