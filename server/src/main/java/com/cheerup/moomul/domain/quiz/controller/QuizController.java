b321a6e02c4e1e059ae1eb958f2514b4a146a101
나호균 <nahk99@naver.com>
2024-05-08 16:36:08 +0900

    Feat : ✨ 퀴즈 결과, 다음퀴즈 불러오기

--- server/src/main/java/com/cheerup/moomul/domain/quiz/controller/QuizController.java
+++ server/src/main/java/com/cheerup/moomul/domain/quiz/controller/QuizController.java
@@ -1,30 +1,39 @@
 package com.cheerup.moomul.domain.quiz.controller;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cheerup.moomul.domain.member.entity.User;
+import com.cheerup.moomul.domain.member.repository.UserRepository;
 import com.cheerup.moomul.domain.quiz.dto.CancelRequestDto;
 import com.cheerup.moomul.domain.quiz.dto.CreateRequestDto;
 import com.cheerup.moomul.domain.quiz.dto.JoinRequestDto;
+import com.cheerup.moomul.domain.quiz.dto.QuizResponseDto;
 import com.cheerup.moomul.domain.quiz.dto.ResultRequestDto;
+import com.cheerup.moomul.domain.quiz.dto.ResultResponseDto;
 import com.cheerup.moomul.domain.quiz.dto.SubmitRequestDto;
+import com.cheerup.moomul.domain.quiz.entity.Participant;
+import com.cheerup.moomul.domain.quiz.entity.Party;
+import com.cheerup.moomul.domain.quiz.entity.Quiz;
+import com.cheerup.moomul.domain.quiz.entity.QuizInfo;
+import com.cheerup.moomul.domain.quiz.entity.Rank;
+import com.cheerup.moomul.domain.quiz.entity.Room;
 import com.cheerup.moomul.domain.quiz.repository.PartyRepository;
 import com.cheerup.moomul.domain.quiz.repository.QuizInfoRepository;
 import com.cheerup.moomul.domain.quiz.repository.RoomRepository;
 import com.cheerup.moomul.domain.quiz.service.QuizService;
+import com.cheerup.moomul.global.response.BaseException;
+import com.cheerup.moomul.global.response.ErrorCode;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -36,6 +45,34 @@ public class QuizController {
 	private final QuizService quizService;
 	private final RoomRepository roomRepository;
 	private final PartyRepository partyRepository;
+	private final UserRepository userRepository;
+
+	@PostConstruct
+	public void init(){
+		//퀴즈 세팅
+		List<Quiz> list=new ArrayList<>();
+		list.add(new Quiz(1L,"1번","보기1","보기2"));
+		list.add(new Quiz(2L,"2번","보기1","보기2"));
+		list.add(new Quiz(3L,"3번","보기1","보기2"));
+		list.add(new Quiz(4L,"4번","보기1","보기2"));
+		list.add(new Quiz(5L,"5번","보기1","보기2"));
+		list.add(new Quiz(6L,"6번","보기1","보기2"));
+		list.add(new Quiz(7L,"7번","보기1","보기2"));
+		quizInfoRepository.save(new QuizInfo(1L,1,list));
+
+		//방정보 세팅
+		roomRepository.save(new Room(1L,6,7,false,"나호균"));
+
+		//참여자 세팅
+		List<Participant> partyList=new ArrayList<>();
+		partyList.add(new Participant("나호균",0));
+		partyList.add(new Participant("2user",10));
+		partyList.add(new Participant("3user",20));
+		partyList.add(new Participant("4user",30));
+		partyList.add(new Participant("5user",40));
+		partyList.add(new Participant("6user",50));
+		partyRepository.save(new Party(1L,partyList));
+	}
 	
 	@MessageMapping("/quiz/{userId}/create")
 	public CreateRequestDto create(@DestinationVariable Long userId, CreateRequestDto createRequestDto) {
@@ -74,10 +111,28 @@ public class QuizController {
 	@MessageMapping("/quiz/{userId}/next")
 	public void next(@DestinationVariable Long userId) {
 
+		QuizResponseDto cur=quizService.findNextQuiz(userId);
+
+		Party curParty=partyRepository.findById(userId).get();
+
+		for(Participant toSender:curParty.getParticipants()){
+			sendingOperations.convertAndSend("/sub/quiz/"+userId+"/"+toSender.getNickname(),cur);
+		}
+
 	}
 
 	@MessageMapping("/quiz/{userId}/result")
 	public void result(@DestinationVariable Long userId, ResultRequestDto resultRequestDto) {
+		List<Rank> result=quizService.getResult(userId,resultRequestDto.nickname());
+		Rank myRank=null;
+		for(Rank cur: result){
+			if(cur.getNickname().equals(resultRequestDto.nickname())){
+				myRank=cur;
+			}
+		}
+		ResultResponseDto resultResponseDto=new ResultResponseDto("result",myRank,result);
+
+		sendingOperations.convertAndSend("/sub/quiz/"+userId+"/"+resultRequestDto.nickname(),resultResponseDto);
 
 	}
 
--- server/src/main/java/com/cheerup/moomul/domain/quiz/service/QuizService.java
+++ server/src/main/java/com/cheerup/moomul/domain/quiz/service/QuizService.java
@@ -1,12 +1,23 @@
 package com.cheerup.moomul.domain.quiz.service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cheerup.moomul.domain.quiz.dto.NextQuizDto;
+import com.cheerup.moomul.domain.quiz.dto.QuizResponseDto;
+import com.cheerup.moomul.domain.quiz.entity.Participant;
 import com.cheerup.moomul.domain.quiz.entity.Quiz;
+import com.cheerup.moomul.domain.quiz.entity.QuizInfo;
+import com.cheerup.moomul.domain.quiz.entity.Rank;
+import com.cheerup.moomul.domain.quiz.entity.Room;
+import com.cheerup.moomul.domain.quiz.repository.PartyRepository;
+import com.cheerup.moomul.domain.quiz.repository.QuizInfoRepository;
 import com.cheerup.moomul.domain.quiz.repository.QuizRepository;
+import com.cheerup.moomul.domain.quiz.repository.RoomRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,8 +27,40 @@ import lombok.RequiredArgsConstructor;
 public class QuizService {
 
 	private final QuizRepository quizRepository;
+	private final QuizInfoRepository quizInfoRepository;
+	private final RoomRepository roomRepository;
+	private final PartyRepository partyRepository;
 
 	public List<Quiz> findRandomQuiz(int limit) {
 		return quizRepository.findRandomQuiz(limit);
+	}
+
+	public QuizResponseDto findNextQuiz(Long userId) {
+		QuizInfo quizInfo=quizInfoRepository.findById(userId).get();
+
+		int curQuizNum=quizInfo.getCurQuizNum()-1;
+		Room curRoom=roomRepository.findById(userId).get();
+		int numOfQuiz=curRoom.getNumOfQuiz();
+		Quiz curQuiz=quizInfo.getQuizList().get(curQuizNum);
+		NextQuizDto nextQuiz=new NextQuizDto(curQuiz.getQuestion(), curQuiz.getOption1(), curQuiz.getOption2());
+
+		quizInfoRepository.delete(quizInfo);
+		quizInfoRepository.save(new QuizInfo(quizInfo.getUserId(), quizInfo.getCurQuizNum()+1,quizInfo.getQuizList()));
+
+		return new QuizResponseDto("nextQuiz", nextQuiz, curQuizNum, numOfQuiz, LocalDateTime.now().plusSeconds(30L));
+	}
+
+	public List<Rank> getResult(Long userId, String nickname) {
+		List<Participant> curParty=partyRepository.findById(userId).get().getParticipants();
+
+		curParty.sort((o1,o2)->Integer.compare(o2.getScore(),o1.getScore()));
+
+		List<Rank> curRank=new ArrayList<>();
+		int idx=1;
+		for(Participant cur:curParty) {
+			curRank.add(new Rank(idx++,cur.getNickname(),cur.getScore()));
+		}
+
+		return curRank;
 	}
 }
