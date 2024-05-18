24eb13303bd98f06b1d317415e0942a2632537fb
정유경 <yyk834@naver.com>
2024-05-18 17:49:12 +0900

    Test : ☔️ ToMeController 테스트코드 - 댓글작성

--- server/src/test/java/com/cheerup/moomul/domain/post/controller/ToMeControllerTest.java
+++ server/src/test/java/com/cheerup/moomul/domain/post/controller/ToMeControllerTest.java
@@ -13,16 +13,22 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.transaction.AfterTransaction;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import com.cheerup.moomul.domain.member.entity.LoginRequestDto;
+import com.cheerup.moomul.domain.member.entity.LoginResponseDto;
 import com.cheerup.moomul.domain.member.entity.User;
 import com.cheerup.moomul.domain.member.repository.UserRepository;
+import com.cheerup.moomul.domain.post.dto.CommentRequestDto;
 import com.cheerup.moomul.domain.post.dto.CommentResponseDto;
 import com.cheerup.moomul.domain.post.entity.Comment;
 import com.cheerup.moomul.domain.post.entity.Post;
@@ -112,5 +118,38 @@ class ToMeControllerTest {
 
 	@Test
 	void postComments() {
+		//Given
+		LoginRequestDto loginRequestDto = new LoginRequestDto("늘보", "늘보");
+		HttpHeaders loginHeaders = new HttpHeaders();
+		loginHeaders.setContentType(MediaType.APPLICATION_JSON);
+		HttpEntity<LoginRequestDto> LoginRequestEntity = new HttpEntity<>(loginRequestDto, loginHeaders);
+
+		ResponseEntity<LoginResponseDto> login = restTemplate.exchange(
+			"/users/login",
+			HttpMethod.POST,
+			LoginRequestEntity,
+			new ParameterizedTypeReference<LoginResponseDto>() {
+			}
+		);
+
+		String userToken = login.getBody().accessToken();
+		CommentRequestDto comment = new CommentRequestDto(
+			null, "댓글작성");
+		HttpHeaders headers = new HttpHeaders();
+		headers.setBearerAuth(userToken);
+		HttpEntity<CommentRequestDto> requestEntity = new HttpEntity<>(comment, headers);
+
+		//when
+		ResponseEntity<List<CommentResponseDto>> response = restTemplate.exchange(
+			"/tome/1/comments",
+			HttpMethod.POST,
+			requestEntity,
+			new ParameterizedTypeReference<List<CommentResponseDto>>() {
+			}
+		);
+
+		// Then
+		assertEquals(HttpStatus.ACCEPTED, response.getStatusCode());
+		assertThat(Objects.requireNonNull(response.getBody()).size()).isEqualTo(3);
 	}
 }
